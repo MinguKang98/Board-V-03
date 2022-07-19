cd633818056ca32b1d73f3cc0486a1b03f586e4e
mingu <alsrn7590@naver.com>
2022-07-19 17:49:57 +0900

    Board-Controller/메서드 추가
    boardView 생성

--- src/main/java/com/study/boardv03/controller/BoardController.java
+++ src/main/java/com/study/boardv03/controller/BoardController.java
@@ -1,18 +1,20 @@
 package com.study.boardv03.controller;
 
+import com.study.boardv03.controller.dto.BoardWriteDto;
 import com.study.boardv03.criteria.PagingCriteria;
 import com.study.boardv03.criteria.SearchCriteria;
 import com.study.boardv03.domain.Board;
 import com.study.boardv03.domain.Category;
+import com.study.boardv03.domain.Comment;
+import com.study.boardv03.domain.File;
 import com.study.boardv03.service.BoardService;
 import com.study.boardv03.service.CategoryService;
+import com.study.boardv03.service.CommentService;
+import com.study.boardv03.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,18 +29,20 @@ public class BoardController {
 
     private final BoardService boardService;
     private final CategoryService categoryService;
+    private final CommentService commentService;
+    private final FileService fileService;
 
     /**
      *
-     * @param curPage
-     * @param searchCriteria
+     * @param curPage : 현재 페이지
+     * @param searchCriteria : 검색 조건
      * @param model
      * @return
      */
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String boardList( @RequestParam(value = "curPage", defaultValue = "1") int curPage,
                              @ModelAttribute SearchCriteria searchCriteria,
-                             Model model) {
+                             Model model ) {
 
         int totalBoardCount = boardService.getTotalBoardCountBySearchCriteria(searchCriteria);
         PagingCriteria pagingCriteria = new PagingCriteria(curPage, totalBoardCount);
@@ -56,9 +60,37 @@ public class BoardController {
         return "list";
     }
 
-    //TODO 게시글-보기
+    /**
+     *
+     *
+     * @param boardId : Baord의 boardId
+     * @param curPage : 목록에서의 현재 페이지
+     * @param searchCriteria : 목록에서의 검색 조건
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/view/{boardId}", method = RequestMethod.GET)
+    public String boardView( @PathVariable("boardId") int boardId,
+                             @RequestParam(value = "curPage", defaultValue = "1") int curPage,
+                             @ModelAttribute SearchCriteria searchCriteria,
+                             Model model) {
 
-    //TODO 게시글-작성
+        Board board = boardService.getBoardById(boardId);
+        Category category = categoryService.getCategoryById(board.getCategoryId());
+        List<Comment> commentList = commentService.getCommentListByBoardId(boardId);
+        List<File> fileList = fileService.getFileListByBoardId(boardId);
+
+        model.addAttribute("searchCriteria", searchCriteria);
+        model.addAttribute("curPage", curPage);
+        model.addAttribute("board", board);
+        model.addAttribute("category", category);
+        model.addAttribute("commentList", commentList);
+        model.addAttribute("fileList", fileList);
+
+        return "view";
+    }
+
+    //TODO 게시글 작성
 
     //TODO 게시글-작성 처리
 
